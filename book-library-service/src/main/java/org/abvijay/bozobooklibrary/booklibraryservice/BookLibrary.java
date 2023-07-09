ab6dccf5c295015a7c7c20b3c9aea1f940886f5a
Hossam Kandel <hossam20052@hotmail.com>
2023-07-09 20:11:45 +0400

    add to string

--- book-library-service/src/main/java/org/abvijay/bozobooklibrary/booklibraryservice/BookLibrary.java
+++ book-library-service/src/main/java/org/abvijay/bozobooklibrary/booklibraryservice/BookLibrary.java
@@ -32,5 +32,10 @@ public class BookLibrary extends PanacheEntityBase implements Serializable{
 		this.userID = userID;
 	}
 
+	@Override
+	public String toString() {
+		return "BookLibrary [bookID=" + bookID + ", userID=" + userID + "]";
+	}
+
     
 }
