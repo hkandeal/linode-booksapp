ecbc9557c68f82066a883864d8890a4b5469c86a
Hossam Kandel <hossam20052@hotmail.com>
2023-07-10 23:04:32 +0400

    add more

--- book-library-service/src/main/java/org/abvijay/bozobooklibrary/booklibraryservice/BookLibrary.java
+++ book-library-service/src/main/java/org/abvijay/bozobooklibrary/booklibraryservice/BookLibrary.java
@@ -37,5 +37,36 @@ public class BookLibrary extends PanacheEntityBase implements Serializable{
 		return "BookLibrary [bookID=" + bookID + ", userID=" + userID + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bookID == null) ? 0 : bookID.hashCode());
+		result = prime * result + ((userID == null) ? 0 : userID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookLibrary other = (BookLibrary) obj;
+		if (bookID == null) {
+			if (other.bookID != null)
+				return false;
+		} else if (!bookID.equals(other.bookID))
+			return false;
+		if (userID == null) {
+			if (other.userID != null)
+				return false;
+		} else if (!userID.equals(other.userID))
+			return false;
+		return true;
+	}
+
     
 }
